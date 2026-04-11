737339e95580f47f43402f8b5ccaaec69d101733
SinghSourav78 <saurabhkumar2364@gmail.com>
2026-04-11 11:33:56 +0530

    Initial set of Typescript

---
+++ Day2/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}! Welcome to TypeScript!`;
+}
+const userName = "Sourav";
+console.log(greet(userName));
+//# sourceMappingURL=index.js.map
